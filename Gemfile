7d44d0b748cd2419e1fc24026b2b163e778514fc
Gergely Bekesi <gergely.bekesi@bitrise.io>
2018-11-19 17:18:41 +0100

    Implement sample app WIP

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'rack'
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'thin'
--- app.rb
+++ app.rb
@@ -1,7 +1,66 @@
-require 'sinatra'
+require File.dirname(__FILE__) + '/data_store'
+require 'sinatra/base'
+require 'sinatra/reloader'
 
-set :bind, '0.0.0.0'
+class App < Sinatra::Base
+  set :bind, '0.0.0.0'
 
-get '/' do
-    'Hello World!'
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  data_store = DataStore.new
+
+  get '/' do
+    'Welcome to Bitrise Sample Addon'
+  end
+
+  post '/provision' do
+    request.body.rewind
+    request_payload = JSON.parse(request.body.read)
+    access_token = data_store.provision_addon_for_app(request_payload['app_slug'], request_payload['plan'], SecureRandom.hex(32))
+    {envs: [{"ACCESS_TOKEN": access_token}]}.to_json
+  end
+
+  patch '/:app_slug/provision' do
+    if !data_store.authenticate(params['app_slug'], request.env['HTTP_AUTHORIZATION'])
+      error 401
+    end
+    request.body.rewind
+    request_payload = JSON.parse(request.body.read)
+
+    begin
+      data_store.update_plan!(params['app_slug'], request_payload["plan"])
+      {message: 'ok'}.to_json
+    rescue StandardError => ex
+      status 400
+      return {message: ex.to_s}.to_json
+    end
+  end
+
+  delete '/provision' do
+  end
+
+  post '/login' do
+  end
+
+  get '/:app_slug/ascii-art' do
+    if !data_store.authenticate(params['app_slug'], request.env['HTTP_AUTHORIZATION'])
+      error 401
+    end
+
+    begin
+      data_store.check_limit!(params['app_slug'])
+    rescue StandardError => ex
+      status 400
+      return {message: ex.to_s}.to_json
+    end
+
+    file = File.open('assets/bitbot.txt')
+    bitbot = []
+    file.each do |line|
+      bitbot = bitbot.concat([line.to_s])
+    end
+    bitbot.join()
+  end
 end
