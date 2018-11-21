4829f284caf902d3efd3918ff50bc47b57ac1260
Gergely Bekesi <gergely.bekesi@bitrise.io>
2018-11-21 17:46:23 +0100

    Add App struct to application

--- data_store.rb
+++ data_store.rb
@@ -1,27 +1,34 @@
 class DataStore
+  App = Struct.new(:slug, :plan, :api_token){}
+
   def initialize
-    @provisioned_apps = {}
+    @provisioned_apps = []
     @request_numbers = {}
   end
 
   def provision_addon_for_app!(app_slug, plan, api_token)
     raise 'The requested plan is not available for the addon!' if !valid_plan?(plan)
-    return @provisioned_apps[app_slug] if @provisioned_apps.keys.include?(app_slug)
-    @provisioned_apps[app_slug] = {api_token: api_token, plan: plan}
-    return @provisioned_apps[app_slug]
+    app = get_app(app_slug)
+    return app if app != nil
+    @provisioned_apps << App.new(app_slug, plan, api_token)
+    return @provisioned_apps.last
   end
 
   def get_app(app_slug)
-    return @provisioned_apps[app_slug]
+    @provisioned_apps.each do |app|
+      return app if app.slug == app_slug
+    end
+    return nil
   end
 
   def deprovision_addon_for_app(app_slug)
-    @provisioned_apps.delete(app_slug)
+    @provisioned_apps.delete_if {|app| app.slug == app_slug }
   end
 
   def update_plan!(app_slug, plan)
     raise 'The requested plan is not available for the addon!' if !valid_plan?(plan)
-    return @provisioned_apps[app_slug][:plan] = plan
+    app = get_app(app_slug)
+    return app.plan = plan
   end
 
   def valid_plan?(plan)
@@ -29,7 +36,8 @@ class DataStore
   end
 
   def check_limit!(app_slug)
-    return if @provisioned_apps[app_slug]&.[](:plan) == 'unlimited'
+    app = get_app(app_slug)
+    return if app.plan == 'unlimited'
     @request_numbers[app_slug] = 0 if !@request_numbers[app_slug]
     if @request_numbers[app_slug] >= 5
       raise 'No more request is enabled, upgrade to `unlimited` plan for more'
